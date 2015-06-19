6c8fecf8a3921d5103492fd1f24b4c2b297ae03b
Evan Cowden <evan38109@gmail.com>
2015-06-18 20:18:26 -0500

    cleans, lints, reformats and adds a first test

---
+++ test/fixtures/b.js
@@ -0,0 +1,5 @@
+module.exports = function bFactory() {
+  return {
+    description: 'b'
+  };
+};
---
+++ test/indexSpec.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+var path = require('path');
+var plutoPath = require('../lib/index');
+
+describe('index', function() {
+
+  it('maps js files in the root of the search path to modules', function() {
+    var fixturesPath = path.join(__dirname, 'fixtures');
+    var plutoModule = plutoPath.createModuleFromPaths([fixturesPath]);
+    var a = plutoModule.get('a');
+    expect(a).to.be.an('object');
+    expect(a.b).to.eql(require('./fixtures/b')());
+  });
+
+});
