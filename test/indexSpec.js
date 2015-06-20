968da37ab02f926d4e937952104753dd07b76d4e
Evan Cowden <evan38109@gmail.com>
2015-06-20 16:22:28 -0500

    makes plutoPath export a function instead of an object

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -6,7 +6,7 @@ describe('index', function() {
 
   it('maps js files in the root of the search path to modules', function() {
     var fixturesPath = path.join(__dirname, 'fixtures');
-    return plutoPath.createModuleFromPaths([fixturesPath]).then(function(plutoModule) {
+    return plutoPath([fixturesPath]).then(function(plutoModule) {
       var a = plutoModule.get('a');
       expect(a).to.be.an('object');
       expect(a.b).to.eql(require('./fixtures/b')());
