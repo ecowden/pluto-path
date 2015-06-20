ff3ccdb3696605035d63a9d73f258b3a0e537c5e
Evan Cowden <evan38109@gmail.com>
2015-06-20 16:37:24 -0500

    makes work with options object, string or array

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -1,16 +1,42 @@
 var expect = require('chai').expect;
 var path = require('path');
 var plutoPath = require('../lib/index');
+var fixturesPath = path.join(__dirname, 'fixtures');
 
 describe('index', function() {
 
   it('maps js files in the root of the search path to modules', function() {
-    var fixturesPath = path.join(__dirname, 'fixtures');
     return plutoPath([fixturesPath]).then(function(plutoModule) {
       var a = plutoModule.get('a');
       expect(a).to.be.an('object');
       expect(a.b).to.eql(require('./fixtures/b')());
     });
+  });
+
+  it('accepts a configuration object', function() {
+    return plutoPath({
+      path: fixturesPath
+    }).then(function(plutoModule) {
+      var a = plutoModule.get('a');
+      expect(a).to.be.an('object');
+      expect(a.b).to.eql(require('./fixtures/b')());
+    });
+  });
+
+  it('accepts a array of paths', function() {
+    return plutoPath({
+      path: fixturesPath
+    }).then(function(plutoModule) {
+      var a = plutoModule.get('a');
+      expect(a).to.be.an('object');
+      expect(a.b).to.eql(require('./fixtures/b')());
+    });
+  });
+
+  it('throws a meaningful error if given a bad configuration type', function() {
+    expect(function() {
+      plutoPath(42);
+    }).to.throw(Error, /options must be a string, array or strings or an object/);
   });
 
 });
