77bd080aea5abaed49f4a1851107984ec7210fc9
Evan Cowden <evan38109@gmail.com>
2015-06-20 16:15:36 -0500

    adds standardBindingSelector and test

---
+++ test/standardBindingSelectorSpec.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect;
+var standardBindingSelector = require('../lib/standardBindingSelector');
+
+describe('standardBindingSelector', function() {
+
+  describe('a component that is not a function', function () {
+
+    it('is bound with `toInstance`', function() {
+      var actual = standardBindingSelector('myInstance', {});
+      expect(actual).to.equal('toInstance');
+    });
+
+  });
+
+  describe('a component that is a function', function() {
+
+    it('with a lower-case name is bound with `toFactory`', function() {
+      var actual = standardBindingSelector('myFactory', noop);
+      expect(actual).to.equal('toFunction');
+    });
+
+    it('with an upper-case name is bound with `toContructor`', function() {
+      var actual = standardBindingSelector('MyContructor', noop);
+      expect(actual).to.equal('toConstructor');
+    });
+  });
+
+
+
+});
+
+function noop() {
+
+}
